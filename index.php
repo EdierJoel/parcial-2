655820a6f036152dd8f3305fef7629e7e0683551
gama96 <00266230@red.unid.mx>
2019-11-14 22:16:27 -0500

    eedicion de contactar

--- index.php
+++ index.php
@@ -369,30 +369,23 @@
       </div>
       <div class="row">
         <div class="col-lg-12">
-          <form id="contactForm" name="sentMessage" novalidate="novalidate">
+          <form id="contactForm" name="sentMessage" novalidate="novalidate" action="enviar2.php">
             <div class="row">
               <div class="col-md-6">
                 <div class="form-group">
-                  <input class="form-control" id="name" type="text" placeholder="Nombre *" required="required" data-validation-required-message="Por favor pon tu nombre.">
+
+                  <input class="form-control" id="nombre" type="text" placeholder="Nombre *" required="required" data-validation-required-message="Por favor pon tu nombre.">
                   <p class="help-block text-danger"></p>
+
                 </div>
                 <div class="form-group">
                   <input class="form-control" id="email" type="email" placeholder="Correo electronico *" required="required" data-validation-required-message="Por favor pon tu correo electronico.">
                   <p class="help-block text-danger"></p>
                 </div>
                 <div class="form-group">
-                  <input class="form-control" id="phone" type="tel" placeholder="Telefono *" required="required" data-validation-required-message="Por favor pon tu numero de telefono.">
+                  <input class="form-control" id="mensaje" type="tel" placeholder="Telefono *" required="required" data-validation-required-message="Por favor pon tu numero de telefono.">
                   <p class="help-block text-danger"></p>
                 </div>
-                <div class="form-group">
-                  <input class="form-control" id="name" type="text" placeholder="Ciudad*" required="required" data-validation-required-message="Por favor pon tu ciudad.">
-                  <p class="help-block text-danger"></p>
-                </div>
-                <div class="form-group">
-                  <input class="form-control" id="name" type="text" placeholder="Estado *" required="required" data-validation-required-message="Por favor pon tu Estado.">
-                  <p class="help-block text-danger"></p>
-                </div>
-               
               </div>
               <div class="col-md-6">
                 <div class="form-group">
