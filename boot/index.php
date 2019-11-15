ab35b241bc8f69dabf931a6e33d6048d77541fba
gama96 <00266230@red.unid.mx>
2019-11-15 00:14:39 -0500

    Update index.php

--- boot/index.php
+++ boot/index.php
@@ -142,32 +142,32 @@
     <script>
 
 function validateForm() {
-    // var x =  document.getElementById('name').value;
-    // if (x == "") {
-    //     document.getElementById('status').innerHTML = "Name cannot be empty";
-    //     return false;
-    // }
-    // x =  document.getElementById('email').value;
-    // if (x == "") {
-    //     document.getElementById('status').innerHTML = "Email cannot be empty";
-    //     return false;
-    // } else {
-    //     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    //     if(!re.test(x)){
-    //         document.getElementById('status').innerHTML = "Email format invalid";
-    //         return false;
-    //     }
-    // }
-    // x =  document.getElementById('subject').value;
-    // if (x == "") {
-    //     document.getElementById('status').innerHTML = "Subject cannot be empty";
-    //     return false;
-    // }
-    // x =  document.getElementById('message').value;
-    // if (x == "") {
-    //     document.getElementById('status').innerHTML = "Message cannot be empty";
-    //     return false;
-    // }
+    var x =  document.getElementById('name').value;
+    if (x == "") {
+        document.getElementById('status').innerHTML = "Name cannot be empty";
+        return false;
+    }
+    x =  document.getElementById('email').value;
+    if (x == "") {
+        document.getElementById('status').innerHTML = "Email cannot be empty";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(x)){
+           document.getElementById('status').innerHTML = "Email format invalid";
+            return false;
+        }
+    }
+    x =  document.getElementById('subject').value;
+    if (x == "") {
+        document.getElementById('status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    x =  document.getElementById('message').value;
+    if (x == "") {
+        document.getElementById('status').innerHTML = "Message cannot be empty";
+        return false;
+    }
  //get input field values data to be sent to server
     document.getElementById('status').innerHTML = "Sending...";
     formData = {
